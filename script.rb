6ab18536a025658b3aab2019ed1ea0a44812853c
7minutech <elijohnson1107@gmail.com>
2024-06-27 21:21:00 -0500

    Added method body to substring method

--- script.rb
+++ script.rb
@@ -1,7 +1,16 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(word, words)
-
+    substr_arr = substring_to_arr(word)
+    p substr_arr
+    substr_hash = {}
+    substr_arr.each do |str|
+        matches = count_matches(str,words)
+        if matches > 0
+            substr_hash[str] = matches
+        end
+    end
+    substr_hash
 end
 def substring_to_arr(word)
     substring_arr = []
@@ -19,13 +28,14 @@ def count_matches(substr,words)
     counter = 0
     words_str = words.join(" ")
     words.each do |word|
-        unless words_str.include?(substr)
+        if words.include?(substr)
           if substr == word
             counter+=1
           end
         end
     end
     counter
+end
 
-substrings("below", dictionary)
+p substrings("below", dictionary)
 #expected output { "below" => 1, "low" => 1 }
