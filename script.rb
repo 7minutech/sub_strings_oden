a102b0c1d8f4bb4bb7651c582127961e0542f4cf
7minutech <elijohnson1107@gmail.com>
2024-06-27 20:30:14 -0500

    Added boilerplate
    Added array of words from TOP made substring
    method signature with expected output from
    what will be passed to substring method

---
+++ script.rb
@@ -0,0 +1,7 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, words)
+end
+
+p substrings("below", dictionary)
+#expected output { "below" => 1, "low" => 1 }
